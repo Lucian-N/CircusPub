538906e4599422f2645618fed79cdfbc703b8010
Lucian-N <lucian_niculescu@ymail.com>
2018-03-04 19:47:38 +0200

    Initial commit

---
+++ app/src/main/java/com/example/lucian_pc/circuspub/TitleScreen.java
@@ -0,0 +1,22 @@
+/*
+ * Copyright (C) 2013 The Android Open Source Project
+ * Copyright (C) Icons Designed by Vexels.com
+ */
+
+package com.example.lucian_pc.circuspub;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * Sets view to activity_title_screen
+ */
+
+public class TitleScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_title_screen);
+    }
+}
